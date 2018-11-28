648e7d62844011389be4096bd6b150a9006d5019
Soohyun Kim <shkim7534@gmail.com>
2018-11-28 11:58:51 +0100

    logistics file working in minizinc

---
+++ labs/lab2/2.java
@@ -0,0 +1,8 @@
+int graph_size = 6;
+int start = 1;
+int n_dests = 2;
+int[] dest = {5,6};
+int n_edges = 7;
+int[] from = {1,1,2, 2,3,4, 4};
+int[] to = {2,3,3, 4,5,5, 6};
+int[] cost = {4,2,5,10,3,4,11};
